91ea84fcd548b98ad8d435a62fdfc602696e1ddf
MiriMarku <57598363+MiriMarku@users.noreply.github.com>
2025-08-28 17:15:45 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open('foglio-ore-v1').then(c=>c.addAll([
-    './','index.html','manifest.webmanifest','icon-192.png','icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
+const CACHE='foglio-ore-v3';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
